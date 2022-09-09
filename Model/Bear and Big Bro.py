63adad6a373812dccfe4c3aeae42f18682e38902
Bahaa Kidwany <bahaa.kidwany@gmaol.com>
2022-09-09 10:15:47 +0200

    Main files for project

---
+++ Model/Bear and Big Bro.py
@@ -0,0 +1,12 @@
+
+
+a = int(input(" Enter weight of Limark "))
+b = int(input(" Enter weight of Bobo "))
+noy = 0
+while a <= b:
+    a = a * 3
+    b = b  *2
+    noy = noy + 1
+    
+
+print(noy)
